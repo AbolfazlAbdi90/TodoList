4ae1c17675e9ecf85bc97877f0a190e6e5f59fc0
Abolfazl Abdi <abdiabolfazl@gmail.com>
2026-01-14 00:58:24 -0800

    init

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+
+import Main from './component/main.vue'
+</script>
+
+<template>
+  <Main />
+</template>
---
+++ src/component/main.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="h-dvh w-full flex items-center justify-center bg-red-200">Hello</div>
+</template>
+
+#app {
+  max-width: 1280px;
+}
