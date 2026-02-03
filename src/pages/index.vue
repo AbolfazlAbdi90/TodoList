1cbaf61bd96dc9b25255189d1696585076cdaa0d
Abolfazl Abdi <abdiabolfazl@gmail.com>
2026-02-03 10:31:41 -0800

    feat: style gradient to trello button

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,6 +2,6 @@
 import Main from '@/component/main.vue'
 </script>
 <template>
-  <RouterLink to="/trello"> <div class="bg-blue-400 px-4 py-2 text-white flex justify-center" ><span >click to go trello page    </span></div></RouterLink>
+  <RouterLink to="/trello"> <div class="bg-blue-400 w-50 mx-auto mt-4 rounded-2xl bg-linear-to-r from-purple-400 px-4 py-2 text-white flex justify-center" ><span >click to go trello page    </span></div></RouterLink>
   <Main></Main>
 </template>
