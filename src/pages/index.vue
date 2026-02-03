ae03812677972f99d594c827e8e48f6b732afd99
Abolfazl Abdi <abdiabolfazl@gmail.com>
2026-02-03 10:21:42 -0800

    set style for trello button

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,6 +2,6 @@
 import Main from '@/component/main.vue'
 </script>
 <template>
-  <RouterLink to="/trello">trello</RouterLink>
+  <RouterLink to="/trello"> <div class="bg-blue-400 px-4 py-2 text-white flex justify-center" ><span >click to go trello page    </span></div></RouterLink>
   <Main></Main>
 </template>
